90db1fff725e854bd7370da456b5ede5e17fcedc
shibataka000 <chise.alter.pasta@gmail.com>
2016-11-11 10:53:01 +0900

    Write about license.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="shibataka000",
     author_email="chise.alter.pasta@gmail.com",
     url="https://github.com/shibataka000/colored_slack_log_handler",
-    license="",
+    license="MIT",
     keyword="slack",
     install_requires=["requests"],
     setup_requires=["pytest-runner", "flake8"],
