7dd291cde82a53b3715401fb1251a7cedcbe9427
shibataka000 <chise.alter.pasta@gmail.com>
2018-08-24 22:37:58 +0900

    Update setup.py.

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
+"""
+distutils/setuptools install script.
+"""
+
+import os
 from setuptools import setup, find_packages
+
+
+def find_install_requires():
+    """
+    Find install_requires from requirements.txt
+    """
+    path = os.path.join(os.path.dirname(__file__), "requirements.txt")
+    return [x.strip() for x in open(path) if not x.startswith("#")]
+
 
 setup(
     name="colored_slack_log_handler",
     version="0.0.2",
-    packages=find_packages(),
-    description=("Python color-coding Slack log handler"),
+    description="Python color-coding Slack log handler",
     author="shibataka000",
-    author_email="chise.alter.pasta@gmail.com",
     url="https://github.com/shibataka000/colored_slack_log_handler",
+    packages=find_packages(),
+    install_requires=find_install_requires(),
     license="MIT",
-    keyword="slack",
-    install_requires=["requests"],
-    setup_requires=["pytest-runner", "flake8"],
-    tests_requires=["pytest", "mock"],
+    classifiers=[]
 )
